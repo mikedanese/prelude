6d81d83266bedb9450ac237aa47e4febaab0e9b1
Mike Danese <mikedanese@google.com>
2016-08-11 14:48:32 -0700

    add some customiztion

---
+++ personal/preload/mikes-stuff.el
@@ -0,0 +1,8 @@
+;;; mikes-stuff.el --- Mike's Super Duper Config
+;;
+;;; Commentary:
+;;    last modified 2016-08-11
+;;
+;;; Code:
+(prelude-require-packages '(multiple-cursors))
+;;; mikes-stuff.el ends here
